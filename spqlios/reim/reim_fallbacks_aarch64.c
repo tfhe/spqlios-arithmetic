1c43984bab8d2969796ebf75bdd5ec5c619652ef
sandra_guasch <sandra.guasch@sandboxaq.com>
2023-07-13 10:07:26 +0000

    link libraries and unit tests

---
+++ spqlios/reim/reim_fallbacks_aarch64.c
@@ -0,0 +1,21 @@
+#include "reim_fft_private.h"
+
+EXPORT void reim_fftvec_addmul_fma(const REIM_FFTVEC_ADDMUL_PRECOMP* precomp, double* r, const double* a,
+                                   const double* b) {
+  UNDEFINED();
+}
+EXPORT void reim_fftvec_mul_fma(const REIM_FFTVEC_MUL_PRECOMP* precomp, double* r, const double* a, const double* b) {
+  UNDEFINED();
+}
+
+EXPORT void reim_fft_avx2_fma(const REIM_FFT_PRECOMP* tables, double* data) { UNDEFINED(); }
+EXPORT void reim_ifft_avx2_fma(const REIM_IFFT_PRECOMP* tables, double* data) { UNDEFINED(); }
+
+EXPORT void reim_fft(const REIM_FFT_PRECOMP* tables, double* data) { tables->function(tables, data); }
+EXPORT void reim_ifft(const REIM_IFFT_PRECOMP* tables, double* data) { tables->function(tables, data); }
+EXPORT void reim_fftvec_mul(const REIM_FFTVEC_MUL_PRECOMP* precomp, double* r, const double* a, const double* b) {
+  precomp->function(precomp, r, a, b);
+}
+EXPORT void reim_fftvec_addmul(const REIM_FFTVEC_ADDMUL_PRECOMP* precomp, double* r, const double* a, const double* b) {
+  precomp->function(precomp, r, a, b);
+}
