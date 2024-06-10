03082e25ef658075196ca6992c8725b712abfe2b
Sergiu Carpov <ssmiler@gmail.com>
2024-06-10 13:56:10 +0200

    naming

--- spqlios/reim/reim_fft4_avx_fma.c
+++ spqlios/reim/reim_fft4_avx_fma.c
@@ -5,7 +5,7 @@
 #include "reim_fft_private.h"
 
 __always_inline void reim_ctwiddle_avx_fma(__m128d* ra, __m128d* rb, __m128d* ia, __m128d* ib, const __m128d omre,
-                                   const __m128d omim) {
+                                           const __m128d omim) {
   // rb * omre - ib * omim;
   __m128d rprod0 = _mm_mul_pd(*ib, omim);
   rprod0 = _mm_fmsub_pd(*rb, omre, rprod0);
@@ -23,10 +23,10 @@ __always_inline void reim_ctwiddle_avx_fma(__m128d* ra, __m128d* rb, __m128d* ia
 EXPORT void reim_fft4_avx_fma(double* dre, double* dim, const void* ompv) {
   const double* omp = (const double*)ompv;
 
-  __m128d ra01 = _mm_loadu_pd(dre);
-  __m128d ra23 = _mm_loadu_pd(dre + 2);
-  __m128d ia01 = _mm_loadu_pd(dim);
-  __m128d ia23 = _mm_loadu_pd(dim + 2);
+  __m128d ra0 = _mm_loadu_pd(dre);
+  __m128d ra2 = _mm_loadu_pd(dre + 2);
+  __m128d ia0 = _mm_loadu_pd(dim);
+  __m128d ia2 = _mm_loadu_pd(dim + 2);
 
   // 1
   {
@@ -35,7 +35,7 @@ EXPORT void reim_fft4_avx_fma(double* dre, double* dim, const void* ompv) {
     __m128d omre = _mm_permute_pd(om, 0);
     __m128d omim = _mm_permute_pd(om, 3);
 
-    reim_ctwiddle_avx_fma(&ra01, &ra23, &ia01, &ia23, omre, omim);
+    reim_ctwiddle_avx_fma(&ra0, &ra2, &ia0, &ia2, omre, omim);
   }
 
   // 2
@@ -45,22 +45,22 @@ EXPORT void reim_fft4_avx_fma(double* dre, double* dim, const void* ompv) {
     __m128d omim = _mm_permute_pd(om, 1);    // omim: i,r
     __m128d omre = _mm_xor_pd(om, fft4neg);  // omre: r,-i
 
-    __m128d rb = _mm_unpackhi_pd(ra01, ra23);  // (r0, r1), (r2, r3) -> (r1, r3)
-    __m128d ib = _mm_unpackhi_pd(ia01, ia23);  // (i0, i1), (i2, i3) -> (i1, i3)
-    __m128d ra = _mm_unpacklo_pd(ra01, ra23);  // (r0, r1), (r2, r3) -> (r0, r2)
-    __m128d ia = _mm_unpacklo_pd(ia01, ia23);  // (i0, i1), (i2, i3) -> (i0, i2)
+    __m128d rb = _mm_unpackhi_pd(ra0, ra2);  // (r0, r1), (r2, r3) -> (r1, r3)
+    __m128d ib = _mm_unpackhi_pd(ia0, ia2);  // (i0, i1), (i2, i3) -> (i1, i3)
+    __m128d ra = _mm_unpacklo_pd(ra0, ra2);  // (r0, r1), (r2, r3) -> (r0, r2)
+    __m128d ia = _mm_unpacklo_pd(ia0, ia2);  // (i0, i1), (i2, i3) -> (i0, i2)
 
     reim_ctwiddle_avx_fma(&ra, &rb, &ia, &ib, omre, omim);
 
-    ra01 = _mm_unpackhi_pd(ra, rb);
-    ra23 = _mm_unpackhi_pd(ia, ib);
-    ia01 = _mm_unpacklo_pd(ra, rb);
-    ia23 = _mm_unpacklo_pd(ia, ib);
+    ra0 = _mm_unpacklo_pd(ra, rb);
+    ia0 = _mm_unpacklo_pd(ia, ib);
+    ra2 = _mm_unpackhi_pd(ra, rb);
+    ia2 = _mm_unpackhi_pd(ia, ib);
   }
 
   // 4
-  _mm_storeu_pd(dre, ia01);
-  _mm_storeu_pd(dre + 2, ra01);
-  _mm_storeu_pd(dim, ia23);
-  _mm_storeu_pd(dim + 2, ra23);
+  _mm_storeu_pd(dre, ra0);
+  _mm_storeu_pd(dre + 2, ra2);
+  _mm_storeu_pd(dim, ia0);
+  _mm_storeu_pd(dim + 2, ia2);
 }
--- spqlios/reim/reim_ifft4_avx_fma.c
+++ spqlios/reim/reim_ifft4_avx_fma.c
@@ -21,10 +21,10 @@ __always_inline void reim_invctwiddle_avx_fma(__m128d* ra, __m128d* rb, __m128d*
 EXPORT void reim_ifft4_avx_fma(double* dre, double* dim, const void* ompv) {
   const double* omp = (const double*)ompv;
 
-  __m128d ra01 = _mm_loadu_pd(dre);
-  __m128d ra23 = _mm_loadu_pd(dre + 2);
-  __m128d ia01 = _mm_loadu_pd(dim);
-  __m128d ia23 = _mm_loadu_pd(dim + 2);
+  __m128d ra0 = _mm_loadu_pd(dre);
+  __m128d ra2 = _mm_loadu_pd(dre + 2);
+  __m128d ia0 = _mm_loadu_pd(dim);
+  __m128d ia2 = _mm_loadu_pd(dim + 2);
 
   // 1
   {
@@ -32,17 +32,17 @@ EXPORT void reim_ifft4_avx_fma(double* dre, double* dim, const void* ompv) {
     __m128d omre = _mm_loadu_pd(omp);                              // omre: r,i
     __m128d omim = _mm_xor_pd(_mm_permute_pd(omre, 1), ifft4neg);  // omim: i,-r
 
-    __m128d ra = _mm_unpacklo_pd(ra01, ra23);  // (r0, r1), (r2, r3) -> (r0, r2)
-    __m128d ia = _mm_unpacklo_pd(ia01, ia23);  // (i0, i1), (i2, i3) -> (i0, i2)
-    __m128d rb = _mm_unpackhi_pd(ra01, ra23);  // (r0, r1), (r2, r3) -> (r1, r3)
-    __m128d ib = _mm_unpackhi_pd(ia01, ia23);  // (i0, i1), (i2, i3) -> (i1, i3)
+    __m128d ra = _mm_unpacklo_pd(ra0, ra2);  // (r0, r1), (r2, r3) -> (r0, r2)
+    __m128d ia = _mm_unpacklo_pd(ia0, ia2);  // (i0, i1), (i2, i3) -> (i0, i2)
+    __m128d rb = _mm_unpackhi_pd(ra0, ra2);  // (r0, r1), (r2, r3) -> (r1, r3)
+    __m128d ib = _mm_unpackhi_pd(ia0, ia2);  // (i0, i1), (i2, i3) -> (i1, i3)
 
     reim_invctwiddle_avx_fma(&ra, &rb, &ia, &ib, omre, omim);
 
-    ra01 = _mm_unpacklo_pd(ra, rb);
-    ia01 = _mm_unpacklo_pd(ia, ib);
-    ra23 = _mm_unpackhi_pd(ra, rb);
-    ia23 = _mm_unpackhi_pd(ia, ib);
+    ra0 = _mm_unpacklo_pd(ra, rb);
+    ia0 = _mm_unpacklo_pd(ia, ib);
+    ra2 = _mm_unpackhi_pd(ra, rb);
+    ia2 = _mm_unpackhi_pd(ia, ib);
   }
 
   // 2
@@ -51,12 +51,12 @@ EXPORT void reim_ifft4_avx_fma(double* dre, double* dim, const void* ompv) {
     __m128d omre = _mm_permute_pd(om, 0);
     __m128d omim = _mm_permute_pd(om, 3);
 
-    reim_invctwiddle_avx_fma(&ra01, &ra23, &ia01, &ia23, omre, omim);
+    reim_invctwiddle_avx_fma(&ra0, &ra2, &ia0, &ia2, omre, omim);
   }
 
   // 4
-  _mm_storeu_pd(dre, ra01);
-  _mm_storeu_pd(dre + 2, ra23);
-  _mm_storeu_pd(dim, ia01);
-  _mm_storeu_pd(dim + 2, ia23);
+  _mm_storeu_pd(dre, ra0);
+  _mm_storeu_pd(dre + 2, ra2);
+  _mm_storeu_pd(dim, ia0);
+  _mm_storeu_pd(dim + 2, ia2);
 }
