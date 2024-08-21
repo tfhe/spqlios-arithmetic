7ea875f61c51c67687ec9c15ae0eae04cda961f8
Nicolas Gama <nicolas.gama@gmail.com>
2024-08-21 10:54:39 +0200

    Update version and changelog.

--- Changelog.md
+++ Changelog.md
@@ -3,6 +3,12 @@
 All notable changes to this project will be documented in this file.
 this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [2.0.0] - 2024-08-21
+
+- Initial release of the `vec_znx` (except convolution products), `vec_rnx` and `zn` apis.
+- Hardware acceleration available: AVX2 (most parts)
+- APIs are documented in the wiki and are in "beta mode": during the 2.x -> 3.x transition, functions whose API is satisfactory in test projects will pass in "stable mode".
+
 ## [1.0.0] - 2023-07-18
 
 - Initial release of the double precision fft on the reim and cplx backends
--- scripts/auto-release.sh
+++ scripts/auto-release.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # this script generates one tag if there is a version change in manifest.yaml
-cd `dirname $0`
+cd `dirname $0`/..
 if [ "v$1" = "v-y" ]; then
   echo "production mode!";
 fi
-changes=$(git diff HEAD~1..HEAD -- manifest.yaml | grep 'version:')
+changes=`git diff HEAD~1..HEAD -- manifest.yaml | grep 'version:'`
 oldversion=$(echo "$changes" | grep '^-version:' | cut '-d ' -f2)
 version=$(echo "$changes" | grep '^+version:' | cut '-d ' -f2)
 echo "Versions: $oldversion --> $version"
@@ -18,7 +18,7 @@ if [ "v$1" = "v-y" ]; then
   git tag -a "v$version" -m "Version $version"
   git push origin "v$version"
 else
-echo <<EOF
+cat <<EOF
   # the script would do:
   git tag -a "v$version" -m "Version $version"
   git push origin "v$version"
